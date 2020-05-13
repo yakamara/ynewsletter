e6b03446d5a43c4fca001044f17105aa93bd49fc
Thomas Blum <thomas@redaxo.org>
2020-05-13 16:08:00 +0200

    \r und andere Zeichen vermeiden/löschen

--- lib/ynewsletter_group.php
+++ lib/ynewsletter_group.php
@@ -9,7 +9,7 @@ class rex_ynewsletter_group extends \rex_yform_manager_dataset
         $group_filters = trim($this->filter);
         if ($group_filters != "") {
             foreach(explode("\n", $group_filters) as $group_filter) {
-                $filter[] = '('.$group_filter.')';
+                $filter[] = '('.trim($group_filter).')';
             }
             $query .= ' where ' . implode(' and ', $filter);
         }
