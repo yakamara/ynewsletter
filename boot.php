847f54f1a2859742c28e426bd75e434308356459
Jan Kristinus <jan.kristinus@yakamara.de>
2020-10-20 12:43:37 +0200

    Unnötige Navigation Newsletter Blockdefinitionen entfernt

--- boot.php
+++ boot.php
@@ -1,24 +1,5 @@
 <?php
 
-if (rex::isBackend()) {
-    rex_extension::register('PACKAGES_INCLUDED', static function ($params) {
-        $plugin = rex_plugin::get('yform', 'manager');
-
-        if ($plugin) {
-            $pages = $plugin->getProperty('pages');
-            $ycom_tables = ['rex_ynewsletter', 'rex_ynewsletter_log', 'rex_ynewsletter_group'];
-
-            if (isset($pages) && is_array($pages)) {
-                foreach ($pages as $page) {
-                    if (in_array($page->getKey(), $ycom_tables)) {
-                        $page->setBlock('ynewsletter');
-                    }
-                }
-            }
-        }
-    });
-}
-
 rex_yform_manager_dataset::setModelClass('rex_ynewsletter', rex_ynewsletter::class);
 rex_yform_manager_dataset::setModelClass('rex_ynewsletter_log', rex_ynewsletter_log::class);
 rex_yform_manager_dataset::setModelClass('rex_ynewsletter_group', rex_ynewsletter_group::class);
