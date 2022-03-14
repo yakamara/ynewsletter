e926ec3e49ac5954c2bf9caf65bd273b6737094f
Jan Kristinus <jan.kristinus@yakamara.de>
2022-03-15 00:39:15 +0100

    YForm 4 Anpassungen choice und select #46

--- install.php
+++ install.php
@@ -1,4 +1,17 @@
 <?php
 
+rex_sql::factory()->setQuery(
+    'update
+                `' . rex::getTable('yform_field') . '`
+           set
+                type_name = "choice",
+                db_type = "text",
+                expanded = 0,
+                choices = options
+           where
+                table_name LIKE "rex_ynewsletter%" and
+                type_name="select"'
+);
+
 $content = rex_file::get(rex_path::addon('ynewsletter', 'install/tablesets/ynewsletter_tables.json'));
 rex_yform_manager_table_api::importTablesets($content);
