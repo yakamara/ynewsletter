c45af3021b741a3cc211e7d17e39f9760762ba8c
Jan Kristinus <jan.kristinus@yakamara.de>
2022-03-15 01:05:39 +0100

    install angepasst, alte select felder werden gelöscht

--- install.php
+++ install.php
@@ -1,17 +1,11 @@
 <?php
 
 rex_sql::factory()->setQuery(
-    'update
+    'delete from
                 `' . rex::getTable('yform_field') . '`
-           set
-                type_name = "choice",
-                db_type = "text",
-                expanded = 0,
-                choices = options
            where
                 table_name LIKE "rex_ynewsletter%" and
-                type_name="select"'
-);
+                type_name="select"');
 
 $content = rex_file::get(rex_path::addon('ynewsletter', 'install/tablesets/ynewsletter_tables.json'));
 rex_yform_manager_table_api::importTablesets($content);
